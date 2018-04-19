2a1a16b00a37740047897e19dff94dac8cbc5445
Brenda Franco and Julia Sheremet <brenda@>
2018-04-19 15:46:54 -0700

    run three spec tests in karma

--- spec/character-spec.js
+++ spec/character-spec.js
@@ -3,8 +3,37 @@ import { Character } from './../src/character.js';
 describe('Character', function() {
 
   it('should return name of Character', function() {
-    let alanGrant = new Character("Alan Grant", 7, 2, 8, "level 1");
-    let characterAlan = alanGrant.getName();
-    expect(characterAlan).toEqual("Alan Grant");
+    let alanGrant = new Character("Alan Grant", ["knife", "hat"], 7, 5, 10, "level 1");
+    let alanName = alanGrant.getName();
+    expect(alanName).toEqual("Alan Grant");
   });
+
+  // it('should return strength of Character', function() {
+  //   let alanGrant = new Character("Alan Grant", 7, 2, 8, "level 1", ["knife", "hat"]);
+  //   let alanName = alanGrant.getName();
+  //   let alanStrength = alanGrant.getStrength();
+  //   let alanCharacter = `Your Character is: ${alanName} and your strength is: ${alanStrength}.`
+  //   expect(alanCharacter).toEqual("Your Character is: Alan Grant and your strength is: 7.");
+  // });
+
+  it('should return character details', function(){
+    let mario = new Character("Mario", ["mushroom", "flight suit"], 7, 5, 10, "level 2");
+    let marioItems = mario.getItems();
+    expect(marioItems).toEqual(["mushroom", "flight suit"]);
+  });
+
+  it('should return Characters charisma', function(){
+    let mario = new Character("Mario", ["mushroom", "flight suit"], 7, 5, 10, "level 2");
+    let marioCharisma = mario.getCharisma();
+    expect(marioCharisma).toEqual(7);
+  });
+
+  it('should return mushroom if Character finds mushroom', function(){
+    let mario = new Character("Mario", ["mushroom", "flight suit"], 7, 5, 10, "level 2");
+    console.log(mario);
+    let marioMushroom = mario.findMushroom();
+    console.log(marioMushroom);
+    expect(marioMushroom).toEqual(true);
+  });
+
 });
--- src/character.js
+++ src/character.js
@@ -1,14 +1,33 @@
 class Character {
-  constructor(name, strength, charisma, int, level) {
+  constructor(name, items=[], charisma, int, strength, level) {
     this.name = name;
-    this.strength = strength;
-    this.charisma = charisma;
-    this.int = int;
+    this.items = items;
+    this.charisma = 7;
+    this.int = 5;
+    this.strength = 10;
     this.level = level;
   }
 
   getName() {
     return this.name;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getCharisma(){
+    return this.charisma;
+  }
+
+  findMushroom(){
+    let inventoryItems = this.items;
+
+    for (let i = 0; i < inventoryItems.length; i++) {
+      if (inventoryItems[i] === "mushroom") {
+        return true;
+      }
+    }
   }
 }
 
